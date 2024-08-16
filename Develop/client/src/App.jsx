3f3a590b56bb6ed0032c803b9b4ac9803c3aa9b6
Ash53rd <aandersonatx@gmail.com>
2024-08-15 22:20:53 -0500

    tried to fix

--- Develop/client/src/App.jsx
+++ Develop/client/src/App.jsx
@@ -1,14 +1,44 @@
-import './App.css';
-import { Outlet } from 'react-router-dom';
+import "./App.css";
+import { Outlet } from "react-router-dom";
+import {
+  ApolloClient,
+  ApolloProvider,
+  InMemoryCache,
+  createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+import Navbar from "./components/Navbar";
 
-import Navbar from './components/Navbar';
+// Create an HTTP link
+const httpLink = createHttpLink({
+  uri: "/graphql",
+});
+
+// Create a middleware for authentication
+const authLink = setContext((_, { headers }) => {
+  // Get the token from local storage
+  const token = localStorage.getItem("id_token");
+  // Return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+// Create the Apollo Client
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <>
+    <ApolloProvider client={client}>
       <Navbar />
       <Outlet />
-    </>
+    </ApolloProvider>
   );
 }
 
